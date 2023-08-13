faa05a147a1293c9bc23fa5d9b81f992a667de9a
Marcelo <mrcel83@gmail.com>
2023-08-13 08:38:02 +0100

    .

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,7 +1,11 @@
 import "@styles/globals.css"
 
-import Nav from "@components/Nav"
-import Provider from "@components/Provider"
+// import Nav from "@components/Nav"
+// import Provider from "@components/Provider"
+import dynamic from "next/dynamic"
+
+const Nav = dynamic(() => import("@components/Nav"))
+const Provider = dynamic(() => import("@components/Provider"))
 
 export const metadata = {
   title: "Promptopia",
@@ -11,13 +15,16 @@ export const metadata = {
 const RootLayout = ({ children }) => {
   return (
     <html lang='en'>
+      <head>
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
+      </head>
       <body>
-        <Provider>
+        <Provider prefetch={false}>
           <div className='main'>
             <div className='gradient' />
           </div>
           <main className='app'>
-            <Nav />
+            <Nav prefetch={false} />
             {children}
           </main>
         </Provider>
--- app/page.jsx
+++ app/page.jsx
@@ -1,8 +1,9 @@
-import Feed from "@components/Feed"
+import dynamic from "next/dynamic"
+
+const Feed = dynamic(() => import("@components/Feed"))
+// import Feed from "@components/Feed"
 
 const Home = () => {
-  const textFilter = () => {}
-  const imageFilter = () => {}
   return (
     <section className='w-full flex-center flex-col'>
       <h1 className='head_text text-center'>
