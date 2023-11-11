466ebdf944973151539030eef9a1808fd6a4cb0e
m-urculu <mrcel83@gmail.com>
2023-11-11 19:55:46 +0000

    .

--- app/api/prompt/new/route.js
+++ app/api/prompt/new/route.js
@@ -3,10 +3,10 @@ import Prompt from "@models/prompt"
 import { Request, NextResponse } from 'next/server';
 import { revalidateTag } from 'next/cache';
  
-export async function POST(req) {
+export async function POST(Request) {
   revalidateTag('posts'); // Purge all data with the 'posts' tag
   const { userId, title, prompt, tag, img } = await req.json()
-  
+
   try {
     await connectToDB()
     const newPrompt = new Prompt({
@@ -16,14 +16,45 @@ export async function POST(req) {
       tag,
       img,
     })
-    
+
     await newPrompt.save()
-    
-    return NextResponse.json({ 
+
+    return new Response(JSON.stringify({ 
       prompt: newPrompt, 
       revalidated: true,
       now: Date.now()
+    }), {
+      status: 200,
+      headers: {
+        'Content-Type': 'application/json',
+      }
     });
+  } catch (error) {
+    return new Response("Failed to create a new prompt", { status: 500 })
+  }
+}
+
+// export async function POST(Request) {
+//   revalidateTag('posts'); // Purge all data with the 'posts' tag
+//   const { userId, title, prompt, tag, img } = await req.json()
+  
+//   try {
+//     await connectToDB()
+//     const newPrompt = new Prompt({
+//       creator: userId,
+//       title,
+//       prompt,
+//       tag,
+//       img,
+//     })
+    
+//     await newPrompt.save()
+    
+//     return NextResponse.json({ 
+//       prompt: newPrompt, 
+//       revalidated: true,
+//       now: Date.now()
+//     });
     // return new Response(JSON.stringify(newPrompt), { 
     //   status: 201,
     //   headers: {
@@ -32,7 +63,7 @@ export async function POST(req) {
     //     'Pragma': 'no-cache'
     //   }
     //  })
-  } catch (error) {
-    return new Response("Failed to create a new prompt", { status: 500 })
-  }
-}
+//   } catch (error) {
+//     return new Response("Failed to create a new prompt", { status: 500 })
+//   }
+// }
