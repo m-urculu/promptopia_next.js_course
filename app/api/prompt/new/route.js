184e05627208e76c32721e8e9d89ad6c25d28b47
m-urculu <mrcel83@gmail.com>
2023-11-11 20:37:07 +0000

    .

--- app/api/prompt/new/route.js
+++ app/api/prompt/new/route.js
@@ -3,41 +3,10 @@ import Prompt from "@models/prompt"
 import { Request, NextResponse } from 'next/server';
 import { revalidateTag } from 'next/cache';
  
-export async function POST(Request) {
-  revalidateTag('posts'); // Purge all data with the 'posts' tag
-  const { userId, title, prompt, tag, img } = await req.json()
-
-  try {
-    await connectToDB()
-    const newPrompt = new Prompt({
-      creator: userId,
-      title,
-      prompt,
-      tag,
-      img,
-    })
-
-    await newPrompt.save()
-
-    return new Response(JSON.stringify({ 
-      prompt: newPrompt, 
-      revalidated: true,
-      now: Date.now()
-    }), {
-      status: 200,
-      headers: {
-        'Content-Type': 'application/json',
-      }
-    });
-  } catch (error) {
-    return new Response("Failed to create a new prompt", { status: 500 })
-  }
-}
-
 // export async function POST(Request) {
 //   revalidateTag('posts'); // Purge all data with the 'posts' tag
 //   const { userId, title, prompt, tag, img } = await req.json()
-  
+
 //   try {
 //     await connectToDB()
 //     const newPrompt = new Prompt({
@@ -47,23 +16,54 @@ export async function POST(Request) {
 //       tag,
 //       img,
 //     })
-    
+
 //     await newPrompt.save()
-    
-//     return NextResponse.json({ 
+
+//     return new Response(JSON.stringify({ 
 //       prompt: newPrompt, 
 //       revalidated: true,
 //       now: Date.now()
+//     }), {
+//       status: 200,
+//       headers: {
+//         'Content-Type': 'application/json',
+//       }
 //     });
-    // return new Response(JSON.stringify(newPrompt), { 
-    //   status: 201,
-    //   headers: {
-    //     'Content-Type': 'application/json',
-    //     'Cache-Control': 'no-cache',
-    //     'Pragma': 'no-cache'
-    //   }
-    //  })
 //   } catch (error) {
 //     return new Response("Failed to create a new prompt", { status: 500 })
 //   }
 // }
+
+export async function POST(Request) {
+  revalidateTag('posts'); // Purge all data with the 'posts' tag
+  const { userId, title, prompt, tag, img } = await req.json()
+  
+  try {
+    await connectToDB()
+    const newPrompt = new Prompt({
+      creator: userId,
+      title,
+      prompt,
+      tag,
+      img,
+    })
+    
+    await newPrompt.save()
+    
+    return NextResponse.json({ 
+      prompt: newPrompt, 
+      revalidated: true,
+      now: Date.now()
+    });
+    return new Response(JSON.stringify(newPrompt), { 
+      status: 201,
+      headers: {
+        'Content-Type': 'application/json',
+        'Cache-Control': 'no-cache',
+        'Pragma': 'no-cache'
+      }
+     })
+  } catch (error) {
+    return new Response("Failed to create a new prompt", { status: 500 })
+  }
+}
